d71713af0f4917ec6c04f48c42bc1f471af7f4a4
Lucy Mae Tan <lucymae.tan@lsu.edu.ph>
2024-07-15 18:32:33 +0800

    feat: add dynamic card

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
-import React, { useState } from "react";
+import Link from 'next/link';
+import React, { useState } from 'react';
 
 import Card from '@/components/ui/cards';
 
-import Logo from "../Logo";
-import NextImage from "../NextImage";
-import { FlipWords } from "../ui/flip-words";
-import { Boxes } from "../ui/background-boxes";
-
+import Logo from '../Logo';
+import NextImage from '../NextImage';
+import { FlipWords } from '../ui/flip-words';
+import { Boxes } from '../ui/background-boxes';
+import Cards from '@/components/ui/cards';
 
 export function HeroSection() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -15,57 +15,123 @@ export function HeroSection() {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const words = ["PLAY", "CONNECT", "THRIVE"];
+  const words = ['PLAY', 'CONNECT', 'THRIVE'];
+
+  const cardDataArray = [
+    {
+      src: '/images/cards/Catan.jpg',
+      alt: 'Catan Board Game',
+      title: 'Catan',
+      description:
+        'Catan is a multiplayer board game where players build and trade resources to dominate the island.',
+    },
+    {
+      src: '/images/cards/Uno.jpg',
+      alt: 'Ticket to Ride Board Game',
+      title: 'Ticket to Ride',
+      description:
+        'Ticket to Ride is a railway-themed board game where players collect cards of various types of train cars.',
+    },
+    {
+      src: '/images/cards/exploding-kittens.jpg',
+      alt: 'Monopoly Board Game',
+      title: 'Monopoly',
+      description:
+        'Monopoly is a classic board game where players buy, sell, and trade properties to bankrupt their opponents.',
+    },
+    {
+      src: '/images/cards/exploding-kittens.jpg',
+      alt: 'Monopoly Board Game',
+      title: 'Monopoly',
+      description:
+        'Monopoly is a classic board game where players buy, sell, and trade properties to bankrupt their opponents.',
+    },
+    {
+      src: '/images/cards/exploding-kittens.jpg',
+      alt: 'Monopoly Board Game',
+      title: 'Monopoly',
+      description:
+        'Monopoly is a classic board game where players buy, sell, and trade properties to bankrupt their opponents.',
+    },
+    {
+      src: '/images/cards/exploding-kittens.jpg',
+      alt: 'Monopoly Board Game',
+      title: 'Monopoly',
+      description:
+        'Monopoly is a classic board game where players buy, sell, and trade properties to bankrupt their opponents.',
+    },
+   // Add game cards her
+  ];
 
   return (
-    
-    <section className="bg-[#F3F3F3] ">
-           
-<div className="absolute inset-0 w-full bg-[#F3F3F3] z-10 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+    <section className='bg-[#F3F3F3] '>
+      <div className='absolute inset-0 w-full bg-[#F3F3F3] z-10 [mask-image:radial-gradient(transparent,white)] pointer-events-none' />
 
-      <nav className="absolute w-full top-5 py-5 z-30 ">
-        <div className="mx-auto max-w-7xl lg:px-8">
-          <div className="w-full flex flex-col lg:flex-row">
+      <nav className='absolute w-full top-5 py-5 z-30 '>
+        <div className='mx-auto max-w-7xl lg:px-8'>
+          <div className='w-full flex flex-col lg:flex-row'>
             {/* Logo and Menu Button */}
-            <div className="flex justify-between lg:hidden px-4">
-              <Link href="/" aria-label="Meeples" className="flex items-center ">
+            <div className='flex justify-between lg:hidden px-4'>
+              <Link
+                href='/'
+                aria-label='Meeples'
+                className='flex items-center '
+              >
                 <NextImage
                   useSkeleton
-                  src="/images/meeples-wordmark.png"
-                  alt="meeples wordmark"
+                  src='/images/meeples-wordmark.png'
+                  alt='meeples wordmark'
                   width={200}
                   height={115}
                 />
               </Link>
               <button
-                type="button"
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black"
-                aria-controls="mobile-menu"
-                aria-expanded={isMenuOpen ? "true" : "false"}
+                type='button'
+                className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black'
+                aria-controls='mobile-menu'
+                aria-expanded={isMenuOpen ? 'true' : 'false'}
                 onClick={handleToggleMenu}
               >
-                <span className="sr-only">Open main menu</span>
+                <span className='sr-only'>Open main menu</span>
                 {/* Icon when menu is closed. */}
                 <svg
-                  className={isMenuOpen ? "hidden h-6 w-6 cursor-none" : "block h-6 w-6 cursor-none" }
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="black"
-                  aria-hidden="true"
+                  className={
+                    isMenuOpen
+                      ? 'hidden h-6 w-6 cursor-none'
+                      : 'block h-6 w-6 cursor-none'
+                  }
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='black'
+                  aria-hidden='true'
                 >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M4 6h16M4 12h16m-7 6h7'
+                  />
                 </svg>
                 {/* Icon when menu is open. */}
                 <svg
-                  className={isMenuOpen ? "block h-6 w-6 cursor-none" : "hidden h-6 w-6 cursor-none"}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="black"
-                  aria-hidden="true"
+                  className={
+                    isMenuOpen
+                      ? 'block h-6 w-6 cursor-none'
+                      : 'hidden h-6 w-6 cursor-none'
+                  }
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='black'
+                  aria-hidden='true'
                 >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M6 18L18 6M6 6l12 12'
+                  />
                 </svg>
               </button>
             </div>
@@ -74,43 +140,64 @@ export function HeroSection() {
             <div
               className={
                 isMenuOpen
-                  ? "max-sm:bg-white  w-full lg:flex justify-between py-5 max-lg:mt-1 max-lg:px-4 max-lg:shadow-lg max-lg:shadow-gray-200 max-lg:h-auto max-lg:overflow-auto transition-all duration-500 delay-200"
-                  : " max-sm:bg-white hidden w-full lg:flex justify-between py-5 max-lg:mt-1 max-lg:px-4 max-lg:shadow-lg max-lg:shadow-gray-200 max-lg:h-auto max-lg:overflow-auto transition-all duration-500 delay-200"
+                  ? 'max-sm:bg-white  w-full lg:flex justify-between py-5 max-lg:mt-1 max-lg:px-4 max-lg:shadow-lg max-lg:shadow-gray-200 max-lg:h-auto max-lg:overflow-auto transition-all duration-500 delay-200'
+                  : ' max-sm:bg-white hidden w-full lg:flex justify-between py-5 max-lg:mt-1 max-lg:px-4 max-lg:shadow-lg max-lg:shadow-gray-200 max-lg:h-auto max-lg:overflow-auto transition-all duration-500 delay-200'
               }
-              id="navbar"
+              id='navbar'
             >
-              <ul className="flex lg:items-center max-lg:gap-4 max-lg:mb-4 flex-col mt-4 lg:flex-1 md:mt-0 lg:flex-row ">
+              <ul className='flex lg:items-center max-lg:gap-4 max-lg:mb-4 flex-col mt-4 lg:flex-1 md:mt-0 lg:flex-row '>
                 <li>
-                  <a href="#" className="cursor-none text-sm font-semibold leading-6 text-gray-900">
-                    <button className="cursor-none inline-block px-6 py-2.5 text-md font-bold text-center text-black bg-white border-2 border-black rounded-md shadow-[5px_5px_0px_#000] transition-all ease-linear hover:bg-yellow-400 hover:-translate-y-1 active:shadow-none active:translate-y-1 mr-4 btn-tooltip tooltip" data-tooltip="can I join? 🥺">
+                  <a
+                    href='#'
+                    className='cursor-none text-sm font-semibold leading-6 text-gray-900'
+                  >
+                    <button
+                      className='cursor-none inline-block px-6 py-2.5 text-md font-bold text-center text-black bg-white border-2 border-black rounded-md shadow-[5px_5px_0px_#000] transition-all ease-linear hover:bg-yellow-400 hover:-translate-y-1 active:shadow-none active:translate-y-1 mr-4 btn-tooltip tooltip'
+                      data-tooltip='can I join? 🥺'
+                    >
                       Rent
                     </button>
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="cursor-none  text-sm font-semibold leading-6 text-gray-900">
-                    <button className="cursor-none inline-block px-5 py-2.5 text-md font-bold text-center text-black bg-white border-2 border-black rounded-md shadow-[5px_5px_0px_#000] transition-all ease-linear hover:bg-yellow-400 hover:-translate-y-1 active:shadow-none active:translate-y-1 mr-4 btn-tooltip tooltip" data-tooltip="let's play? 😍">
+                  <a
+                    href='#'
+                    className='cursor-none  text-sm font-semibold leading-6 text-gray-900'
+                  >
+                    <button
+                      className='cursor-none inline-block px-5 py-2.5 text-md font-bold text-center text-black bg-white border-2 border-black rounded-md shadow-[5px_5px_0px_#000] transition-all ease-linear hover:bg-yellow-400 hover:-translate-y-1 active:shadow-none active:translate-y-1 mr-4 btn-tooltip tooltip'
+                      data-tooltip="let's play? 😍"
+                    >
                       Games
                     </button>
                   </a>
                 </li>
               </ul>
               {/* Logo */}
-              <div className="hidden lg:flex items-center cursor-none">
+              <div className='hidden lg:flex items-center cursor-none'>
                 <Logo />
               </div>
               {/* Action Buttons */}
-              <div className="flex lg:items-center justify-start flex-col lg:flex-row max-lg:gap-4 lg:flex-1 lg:justify-end">
-                <a href="#" className="cursor-none  text-sm font-semibold leading-6 text-gray-900 tooltip-container">
-                  <button className="cursor-none inline-block px-5 py-2.5 text-md font-bold text-center text-black bg-white border-2 border-black rounded-md shadow-[5px_5px_0px_#000] transition-all ease-linear hover:bg-yellow-400 hover:-translate-y-1 active:shadow-none active:translate-y-1 mr-4 btn-tooltip tooltip" data-tooltip="games! 🎲💛">
+              <div className='flex lg:items-center justify-start flex-col lg:flex-row max-lg:gap-4 lg:flex-1 lg:justify-end'>
+                <a
+                  href='#'
+                  className='cursor-none  text-sm font-semibold leading-6 text-gray-900 tooltip-container'
+                >
+                  <button
+                    className='cursor-none inline-block px-5 py-2.5 text-md font-bold text-center text-black bg-white border-2 border-black rounded-md shadow-[5px_5px_0px_#000] transition-all ease-linear hover:bg-yellow-400 hover:-translate-y-1 active:shadow-none active:translate-y-1 mr-4 btn-tooltip tooltip'
+                    data-tooltip='games! 🎲💛'
+                  >
                     About
                   </button>
                 </a>
                 <a
-                  href="https://discord.gg/vxDY3U8Bwn"
-                  className="text-sm font-semibold leading-6 text-gray-900 cursor-none tooltip-container "
+                  href='https://discord.gg/vxDY3U8Bwn'
+                  className='text-sm font-semibold leading-6 text-gray-900 cursor-none tooltip-container '
                 >
-                  <button className="btn-tooltip tooltip cursor-none inline-block px-5 py-2.5 text-xl font-bold text-center text-white bg-indigo-500 border-2 border-black rounded-md shadow-[5px_5px_0px_#000] transition-all ease-linear transform scale-90 hover:bg-white hover:text-indigo-500 hover:border-indigo-500 hover:shadow-[5px_5px_0px_#6366F1] hover:-translate-y-1 active:bg-indigo-500 active:border-gray-800 active:text-white active:shadow-none active:translate-y-1 " data-tooltip="please join 🥺">
+                  <button
+                    className='btn-tooltip tooltip cursor-none inline-block px-5 py-2.5 text-xl font-bold text-center text-white bg-indigo-500 border-2 border-black rounded-md shadow-[5px_5px_0px_#000] transition-all ease-linear transform scale-90 hover:bg-white hover:text-indigo-500 hover:border-indigo-500 hover:shadow-[5px_5px_0px_#6366F1] hover:-translate-y-1 active:bg-indigo-500 active:border-gray-800 active:text-white active:shadow-none active:translate-y-1 '
+                    data-tooltip='please join 🥺'
+                  >
                     Join Discord
                   </button>
                 </a>
@@ -120,26 +207,31 @@ export function HeroSection() {
         </div>
       </nav>
 
-      <div className="pt-80 h-full flex flex-col items-center justify-center py-20 text-gray-800">
-        <div className="h-screen flex flex-col items-center justify-center flex-1">
-          <div className="h-1/2  flex flex-col items-center justify-center">
-            <h1 className="motto shadow-[5px_5px_0px_#2b2a28] md:text-4xl lg:text-7xl sm:text-xl uppercase relative z-20 font-poppins font-black text-center">
+      <div className='pt-80 h-full flex flex-col items-center justify-center py-20 text-gray-800'>
+        <div className='h-screen flex flex-col items-center justify-center flex-1'>
+          <div className='h-1/2  flex flex-col items-center justify-center'>
+            <h1 className='motto shadow-[5px_5px_0px_#2b2a28] md:text-4xl lg:text-7xl sm:text-xl uppercase relative z-20 font-poppins font-black text-center'>
               <FlipWords words={words} /> <br />
             </h1>
-            <p className="text-center mt-20 font-semibold text-lg relative z-20 max-w-3xl">
-              LSU Meeples - An Interest-Based Organization at La Salle University Ozamiz City Promoting Tabletop Games
+            <p className='text-center mt-20 font-semibold text-lg relative z-20 max-w-3xl'>
+              LSU Meeples - An Interest-Based Organization at La Salle
+              University Ozamiz City Promoting Tabletop Games
             </p>
           </div>
-          <div className="mt-24 flex items-center justify-center z-30">
-          <Card />
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4 bg-gray-100 mt-20 align-middle justify-center items-center'>
+            {cardDataArray.map((card, index) => (
+              <Cards
+                key={index}
+                src={card.src}
+                alt={card.alt}
+                title={card.title}
+                description={card.description}
+              />
+            ))}
+          </div>
         </div>
-        </div>
-
- 
       </div>
-
     </section>
-    
   );
 }
 
--- src/components/ui/cards.tsx
+++ src/components/ui/cards.tsx
@@ -1,32 +1,45 @@
 import React from 'react';
+import NextImage from '../NextImage';
 
-const Cards = () => {
-    return (
+interface CardsProps {
+  src: string;
+  alt: string;
+  title: string;
+  description: string;
+}
 
-<div className="flip-card w-48 h-64 perspective-1000 font-sans">
-<div className="flip-card-inner card">
-  <div className="flip-card-front flex flex-col justify-center items-center">
-  <div className="card-img">
-          <div className="img"> 
-            Test Image
-          </div>
-          </div>
-  </div>
-  <div className="flip-card-back flex flex-col justify-center items-center transform rotate-y-180">
-  <div className="">
-            <div className="card-title text-lg font-medium text-gray-900 mb-3 capitalize">
-                Game Title
+const Cards: React.FC<CardsProps> = ({ src, alt, title, description }) => {
+  return (
+    <div className='flip-card w-48 h-64 perspective-1000 font-sans m-10'>
+      <div className='flip-card-inner card'>
+        <div className='flip-card-front flex flex-col justify-center items-center'>
+          <div className='card-img'>
+            <div className='clip-container'>
+              <NextImage
+                useSkeleton
+                src={src}
+                alt={alt}
+                width={230}
+                height={300}
+                className='object-cover w-full h-full'
+              />
             </div>
-            <div className="card-subtitle text-sm font-normal text-gray-500">
-                Game description. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </div>
+        </div>
+        <div className='flip-card-back flex flex-col justify-center items-center transform rotate-y-180'>
+          <div className=''>
+            <div className='card-title text-lg font-medium text-gray-900 mb-3 capitalize'>
+              {title}
             </div>
-            <hr className="card-divider border-t my-3 border-gray-200" />
-        </div>  
-  </div>
-</div>
-</div>
-        
-    );
+            <div className='card-subtitle text-sm font-normal text-gray-500'>
+              {description}
+            </div>
+            <hr className='card-divider border-t my-3 border-gray-200' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
