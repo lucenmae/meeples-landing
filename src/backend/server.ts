24006e96a32fa8c8bcaf9c220a4a4d5655a80a2d
lucenmae <lucymaetan@gmail.com>
2024-10-05 16:13:33 +0800

    chore: added dotevn

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,11 +1,11 @@
 import cors from 'cors';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 
 import gamesRouter from './routes/games';
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
