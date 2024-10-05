d677508b8a631dd5967b270abd83edfa617277b0
lucenmae <lucymaetan@gmail.com>
2024-10-05 18:30:35 +0800

    fix: uncomment admin pages

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
-import LoginForm from '../../../components/admin/LoginForm';
+// import LoginForm from '../../../components/admin/LoginForm';
 
-export default function AdminLoginPage() {
-  return (
-    <div>
-      <h1>Admin Login</h1>
-      <LoginForm />
-    </div>
-  );
-}
+// export default function AdminLoginPage() {
+//   return (
+//     <div>
+//       <h1>Admin Login</h1>
+//       <LoginForm />
+//     </div>
+//   );
+// }
