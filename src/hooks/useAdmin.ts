da15042a5f2a9155b8a48dc10378dc58640fc162
Lucy Mae Tan <lucymae.tan@lsu.edu.ph>
2024-10-08 20:32:41 +0800

    fix(hooks): correct type usage in useAdmin hook

--- src/hooks/useAdmin.ts
+++ src/hooks/useAdmin.ts
@@ -1,19 +1,8 @@
 import { Session } from 'next-auth';
 import { useSession } from 'next-auth/react';
 
-interface User {
-  name?: string | null;
-  email?: string | null;
-  image?: string | null;
-  role?: string; 
-}
-
-interface CustomSession extends Session {
-  user: User; // Extend the user type
-}
-
 export function useAdmin() {
-  const { data: session, status } = useSession() as { data: CustomSession; status: 'loading' | 'authenticated' | 'unauthenticated' }; // Include status type
+  const { data: session, status } = useSession();
   const isAdmin = session?.user?.role === 'admin';
   const isLoading = status === 'loading';
 
