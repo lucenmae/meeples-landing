91fe9312ff9ee7359ee19f6ad1981cbc435e5501
lucenmae <lucymaetan@gmail.com>
2024-10-23 17:59:12 +0800

    refactor(ui): Remove unused imports and whitespace in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,1 @@
-import { AppProps } from 'next/app';
-import { Toaster } from 'react-hot-toast';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Toaster position="top-right" />
-    </>
-  );
-}
-
-export default MyApp;
+ 
