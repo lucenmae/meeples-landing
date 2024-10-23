f2cfc2fad83d957cec8de14278ad39f4f9c58d11
lucenmae <lucymaetan@gmail.com>
2024-10-23 18:54:55 +0800

    refactor(ui): Remove FollowerPointerCard from Loading component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,11 +1,9 @@
 'use client'
-import { FollowerPointerCard } from '@/components/ui/following-pointer';
 
 const Loading = () => {
 
   return (
-    <FollowerPointerCard>
-      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-white">
         <div className="z-30 absolute top-44 left-0 w-full text-center">
           <h1 className="uppercase font-poppins block font-black text-gray-800 text-4xl md:text-5xl lg:text-6xl">
             Meeples
@@ -14,7 +12,6 @@ const Loading = () => {
         <div className="absolute inset-0 w-full h-full bg-[#F3F3F3] z-10 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
         <div className="loader"></div>
       </div>
-    </FollowerPointerCard>
   );
 };
 
